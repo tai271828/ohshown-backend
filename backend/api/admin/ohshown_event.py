67191175d88aad2a4a5bc1896718e1751da12d03
Taihsiang Ho (tai271828) <e@tai271828.me>
2022-04-24 18:38:25 +0200

    feat(admin): reorder ohshownevent dashboard
    Put the items that researchers care the most at the most lefthand side

--- backend/api/admin/ohshown_event.py
+++ backend/api/admin/ohshown_event.py
@@ -184,14 +184,14 @@ class OhshownEventAdmin(
     list_display = (
         "id",
         "display_number",
+        "ohshown_event_type",
         "sight_see_date_time",
-        "updated_at",
-        "reportrecord_latest_created_at",
         "lat",
         "lng",
-        "ohshown_event_type",
         "source",
         "name",
+        "updated_at",
+        "reportrecord_latest_created_at",
     )
     search_fields = ["townname", "sectname", "display_number"]
     list_filter = (
