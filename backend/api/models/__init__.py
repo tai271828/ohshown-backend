c66ec0bce23db58395552a5f47c302b5b2caa1a9
EagleC <qazx142www@gmail.com>
2022-06-30 14:47:15 +0800

    add bear model and migration

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -5,3 +5,4 @@ from .document import Document, DocumentDisplayStatusEnum, FollowUp
 from .review import Review
 from .gov_agency import GovAgency
 from .trace_form import TraceForm
+from .bear import Bear
